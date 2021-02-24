ade753d5bd86ab75b8c9d45be74909a6d219077e
olavocinacio <olavo1611@gmail.com>
2021-02-24 11:56:04 -0300

    Criação da primeira rota

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,5 +2,18 @@ import express from 'express';
 
 const app = express();
 
+/*
+Métodos de requisição:
+* GET => Buscar
+* POST => Salvar
+* PUT => Alterar
+* DELETE => Deletar
+* PATCH => Alteração específica
+*/
+
+app.get("/users", (request, response) => {
+    return response.send("Hello World - NLW04")
+})
+
 app.listen(3333, () => console.log("Servidor Rodando!!!"));
 
