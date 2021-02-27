12c59c62976a7e50fd8ce7320286f8fc9a13c2ab
olavocinacio <olavo1611@gmail.com>
2021-02-27 15:13:33 -0300

    Criação de um arquivo gerenciador de rotas e conexão do mesmo com o arquivo 'server.ts'

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,25 +1,30 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database" //Quando existe um arquivo index, o projeto reconhece que ele quem deve ser importado
+import { router } from './routes';
 
 const app = express();
 
-/*
-Métodos de requisição:
-* GET => Buscar
-* POST => Salvar
-* PUT => Alterar
-* DELETE => Deletar
-* PATCH => Alteração específica
-*/
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World - NLW04"})
-});
+// /*
+// Métodos de requisição:
+// * GET => Buscar
+// * POST => Salvar
+// * PUT => Alterar
+// * DELETE => Deletar
+// * PATCH => Alteração específica
+// */
 
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"})
-});
+// app.get("/", (request, response) => {
+//     return response.json({message: "Hello World - NLW04"})
+// });
+
+// app.post("/", (request, response) => {
+//     return response.json({message: "Os dados foram salvos com sucesso!"})
+// });
+
+app.use(router);
 
 app.listen(3333, () => console.log("Servidor Rodando!!!"));
 
