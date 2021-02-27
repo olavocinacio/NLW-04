ad0ac91187cc653aa66e8f919cb2bb81348d2251
olavocinacio <olavo1611@gmail.com>
2021-02-27 14:53:38 -0300

    Criando a tabela de usuários

--- api/src/database/migrations/1614435395298-CreateUsers.ts
+++ api/src/database/migrations/1614435395298-CreateUsers.ts
@@ -1,11 +1,37 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1614435395298 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable( //Cria a tabela
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary:true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type:  "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users"); //Excluí a tabela
     }
 
 }
